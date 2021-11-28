7688434a84d14a740380330876cdc9df94281157
newasad <85388826+newasad@users.noreply.github.com>
2021-11-29 01:46:30 +0200

    playing with socket.io

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect ,useRef,useState} from 'react'
 import io from 'socket.io-client'
+import {nanoid} from 'nanoid'
 const socket =io('http://localhost:4000',{ transports : ['websocket'] })
 function App() {
   const [state,setState]=useState("")
+  const [rooms,setRooms]=useState([])
   const stateRef=useRef()
   const inputRef=useRef()
   useEffect(()=>{
     socket.on('start-stream',(helow)=>{
       stateRef.current.innerHTML =`<h1>${helow.helow}</h1>`
     })
+    return ()=>{
+      socket.emit('disconnected',"desconnected true")
+   }
   },[])
   const onEmitter =()=>{
     socket.emit('new-stream',{
