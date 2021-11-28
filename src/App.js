2261ff766c23a340935f92aaefd34e4eb759a2e7
newasad <85388826+newasad@users.noreply.github.com>
2021-11-29 00:07:58 +0200

    started socket.io-client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect ,useRef,useState} from 'react'
+import io from 'socket.io-client'
+const socket =io('http://localhost:4000',{ transports : ['websocket'] })
 function App() {
+  const [state,setState]=useState("")
+  const stateRef=useRef()
+  const inputRef=useRef()
+  useEffect(()=>{
+    socket.on('start-stream',(helow)=>{
+      stateRef.current.innerHTML =`<h1>${helow.helow}</h1>`
+    })
+  },[])
+  const onEmitter =()=>{
+    socket.emit('new-stream',{
+      helow:inputRef.current.value
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p ref={stateRef}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,6 +32,8 @@ function App() {
         >
           Learn React
         </a>
+        <input type="text" ref={inputRef}/>
+        <button onClick={onEmitter}>click</button>
       </header>
     </div>
   );
