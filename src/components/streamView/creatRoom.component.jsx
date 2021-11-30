277d9fa089fd7f9fe619edf55982a162f815a2a8
newasad <85388826+newasad@users.noreply.github.com>
2021-11-30 21:22:01 +0200

    creat rooms  change

--- src/components/streamView/creatRoom.component.jsx
+++ src/components/streamView/creatRoom.component.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import {useNavigate} from 'react-router-dom'
 import { v1 as uuid } from "uuid";
-
+import axios from "axios"
+import './creatRoom.style.css'
 const CreateRoom = () => {
     const navigate =useNavigate();
+    const [rooms,setRooms] = useState([]);
     
-    function create() {
-        const id = uuid();
-        navigate(`/room/${id}`);
+    const  create= ()=> {
+        const options ={
+            headers:{'authorization':`bearer ${JSON.parse(localStorage.getItem('userAccessToken'))}` }
+          }
+        axios.get("https://asad-zoom-look-alike-server.herokuapp.com/auth",options).then((response)=>{
+            if(response.data.role === 'admin'){
+                const id = uuid();
+                setRooms(perv=>[...rooms,`/room/${id}`])
+            }
+        })
+        
     }
     return (
+        <>
+        <ul>
+        {rooms.map((room,index)=>{
+            return(<li className="roomLink"onClick={()=>navigate(room)}>room{index}</li>)
+        })}
+        </ul>
+
         <button onClick={create}>Create room</button>
+        </>
     );
 };
 
