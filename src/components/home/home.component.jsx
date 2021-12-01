d93cb1e66b7d935696cdfb6f843176e0e87f69d1
newasad <85388826+newasad@users.noreply.github.com>
2021-12-01 10:46:36 +0200

    check if the user already in the room

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React,{ useEffect,useState} from 'react';
+import axios from 'axios';
 import "./home.style.css"
 import  CreateRoom  from "../streamView/creatRoom.component";
 import Login from "../login/login.component"
 
 
-const Home=({logedIn,login})=> {
+const Home=({logedIn,setLogedIn,login})=> {
     
+    useEffect(()=>{
+        if(localStorage.getItem('userAccessToken')){
+            (()=>{
+                const options ={
+                    headers:{'authorization':`bearer ${JSON.parse(localStorage.getItem('userAccessToken'))}` }
+                  }
+                axios.get("http://localhost:4000/auth",options).then((response)=>{
+                    setLogedIn(prv=>true)
+                }).catch((error)=>{
+                    console.log(error);
+                    setLogedIn(prv=>false)
+                })
+            }
+            )()
+        }
+        
+    },[])
     return (
         <div>
             {
--- src/components/streamView/creatRoom.component.jsx
+++ src/components/streamView/creatRoom.component.jsx
@@ -1,35 +1,72 @@
-import React,{useEffect,useState} from "react";
-import {useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { v1 as uuid } from "uuid";
-import axios from "axios"
-import './creatRoom.style.css'
-const CreateRoom = () => {
-    const navigate =useNavigate();
-    const [rooms,setRooms] = useState([]);
-    
-    const  create= ()=> {
-        const options ={
-            headers:{'authorization':`bearer ${JSON.parse(localStorage.getItem('userAccessToken'))}` }
-          }
-        axios.get("https://asad-zoom-look-alike-server.herokuapp.com/auth",options).then((response)=>{
-            if(response.data.role === 'admin'){
-                const id = uuid();
-                setRooms(perv=>[...rooms,`/room/${id}`])
-            }
-        })
-        
-    }
-    return (
-        <>
-        <ul>
-        {rooms.map((room,index)=>{
-            return(<li className="roomLink"onClick={()=>navigate(room)}>room{index}</li>)
-        })}
-        </ul>
+import axios from "axios";
+import "./creatRoom.style.css";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:4000", { transports: ["websocket"] });
 
-        <button onClick={create}>Create room</button>
-        </>
-    );
+const CreateRoom = () => {
+  const navigate = useNavigate();
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    (() => {
+      socket.emit("home page refreshed");
+      console.log("home page refreshed");
+      socket.on("all rooms links", (roomsData) => {
+        setRooms(Object.values(roomsData));
+      });
+    })();
+  }, []);
+  const create = () => {
+    const options = {
+      headers: {
+        authorization: `bearer ${JSON.parse(
+          localStorage.getItem("userAccessToken")
+        )}`,
+      },
+    };
+    axios
+      .get("http://localhost:4000/auth", options)
+      .then((response) => {
+        if (response.data.role === "admin") {
+          const id = uuid();
+          setRooms((perv) => [...rooms, { roomID: id, room: `/room/${id}` }]);
+          socket.emit("room created", { roomID: id, roomLink: `/room/${id}` });
+          socket.on("all rooms links", (roomsData) => {
+            setRooms(Object.values(roomsData));
+          });
+        }
+      });
+  };
+  const removeRooms = () => {
+    socket.emit("remove all rooms");
+    socket.on("all rooms links", (roomsData) => {
+      setRooms(Object.values(roomsData));
+    });
+  };
+  return (
+    <>
+      <ul>
+        {rooms &&
+          rooms.map((roomData, index) => {
+            return (
+              <li
+                key={roomData.roomID}
+                className="roomLink"
+                onClick={() => navigate(roomData.roomLink)}
+              >
+                room{index}
+              </li>
+            );
+          })}
+      </ul>
+
+      <button onClick={create}>Create room</button>
+      <button onClick={removeRooms}>Remove All Rooms</button>
+      <button onClick={() => {}}>Log out</button>
+    </>
+  );
 };
 
 export default CreateRoom;
